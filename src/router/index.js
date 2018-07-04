828889976dd75b74f57eed4691f6203a41ead862
ailingao <1058491659@qq.com>
2018-07-04 23:12:33 +0800

    修改默认访问地址

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/components/pages/index'
+import index from '../components/pages/index.vue'
 
 Vue.use(Router)
 
@@ -8,6 +8,11 @@ export default new Router({
     mode: 'history',
     routes: [{
         path: '/index',
+        name: 'index',
+        component: index
+    },
+    {
+        path: '/',
         name: 'index',
         component: index
     }]
